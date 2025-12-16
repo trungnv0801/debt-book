d981a456960a57482e1636c05500dcdce76bef6c
TrungNV-DTS <trungnv@dt-solution.com>
2025-12-16 15:15:04 +0700

    fix: update login screen and app logo

--- src/lang/vi/auth.ts
+++ src/lang/vi/auth.ts
@@ -5,6 +5,14 @@ const auth = {
   password: 'Mật khẩu',
   passwordPlaceholder: '••••••••',
   submit: 'Đăng nhập',
+  forgotPassword: 'Quên mật khẩu?',
+  login: {
+    button: 'Đăng Nhập',
+    loading: 'Đang đăng nhập...',
+  },
+  or: 'hoặc',
+  noAccount: 'Chưa có tài khoản?',
+  signUp: 'Đăng ký ngay',
 }
 
 export default auth
